b7a2b7797fa1538af7ee38398dd70505b8c68e8f
Barakat Bolaji <barakatbolaji65@gmail.com>
2024-02-07 09:37:38 +0100

    - Installing postcss - Imporing app sidebar -Implementing sidebar -Importation of icons - Decorating sidebar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SideBar from "./componenets/SideBar";
+
+const App = () => {
+  return (
+    <div>
+      <SideBar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/componenets/SideBar.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { IoMdHome } from "react-icons/io";
+import { FaRegClock } from "react-icons/fa6";
+import { IoIosNotifications } from "react-icons/io";
+import { CgProfile } from "react-icons/cg";
+import { IoIosChatbubbles } from "react-icons/io";
+import { IoLogOutOutline } from "react-icons/io5";
+
+const links = [
+  {
+    id: 1,
+    title: "Home",
+    to: "/home",
+    Icon: IoMdHome,
+  },
+  {
+    id: 2,
+    title: "Scheduler",
+    to: "/scheduler",
+    Icon: FaRegClock,
+  },
+
+  {
+    id: 3,
+    title: "Notifications",
+    to: "/notifications",
+    Icon: IoIosNotifications,
+  },
+  {
+    id: 4,
+    title: "Profile",
+    to: "/profile",
+    Icon: CgProfile,
+  },
+  {
+    id: 5,
+    title: "Chats",
+    to: "/chat",
+    Icon: IoIosChatbubbles,
+  },
+];
+
+const SideBar = () => {
+  const [nav, SetNavBar] = useState(false);
+
+  return (
+    <div className="min-h-screen p-4 w-48 text-white fixed z-20 md:sticky top md:left-0 top-0 bg-[#07231c]">
+      <ul className=" text-[#07421f]">
+        {links.map(({ id, title, Icon, to }) => (
+          <li
+            key={id}
+            className=" flex gap-4 px-4 cursor-pointer py-6 font-semibold text-white hover:scale-105 duration-300"
+          >
+            <a className="flex gap-4 mb-1 " href={to}>
+              <Icon className="mt-1" />
+              {title}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <button className="text-white bg-[#ff0600] rounded-md py-2 px-6 mt-auto">
+        <icon src={IoLogOutOutline} alt="" />
+        LogOut
+      </button>
+    </div>
+  );
+};
+
+export default SideBar;
